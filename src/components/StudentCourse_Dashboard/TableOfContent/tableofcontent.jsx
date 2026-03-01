331309e49aee88cf33386d9cb6161762308d10fc
Zeeshanarif1 <tpc.zeeshan@gmail.com>
2026-03-01 13:42:38 +0530

    pdfviewform

--- src/components/StudentCourse_Dashboard/TableOfContent/tableofcontent.jsx
+++ src/components/StudentCourse_Dashboard/TableOfContent/tableofcontent.jsx
@@ -1,46 +1,27 @@
-import { useNavigate } from "react-router-dom";
 import "./tableofcontent.css";
+import tocImage from "../../../assests/TOCQ.jpg"; // 👈 apni image yaha import karo
 
 export default function TableOfContent() {
-  const navigate = useNavigate();
-
   return (
     <div className="toc-wrapper">
 
-      {/* TOP IMAGE */}
+      {/* ===== TOP IMAGE ===== */}
       <div className="toc-image">
-        <img
-          src="https://via.placeholder.com/900x250?text=Course+Overview"
-          alt="TOC"
-        />
+        <img src={tocImage} alt="Table of Content" />
       </div>
 
-      {/* CONTENT */}
-      <div className="toc-box">
+      {/* ===== CONTENT BOX ===== */}
+      {/* <div className="toc-box">
         <h2>Table of Content</h2>
 
         <ul>
-          <li >
-            1. Course Details
-          </li>
-
-          <li >
-            2. Numeric Puzzles (PDF)
-          </li>
-
-          <li>
-            3. Quantitative Mock
-          </li>
-
-          <li>
-            4. Reasoning Mock
-          </li>
-
-          <li>
-            5. Verbal Mock
-          </li>
+          <li>1. Course Details</li>
+          <li>2. Numeric Puzzles (PDF)</li>
+          <li>3. Quantitative Mock</li>
+          <li>4. Reasoning Mock</li>
+          <li>5. Verbal Mock</li>
         </ul>
-      </div>
+      </div> */}
 
     </div>
   );
